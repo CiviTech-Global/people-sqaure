5121d5faf3c306808f79f4d315043034ba722d95
Mohammad Khalilzadeh <muhammadkhalilzadeh1998@gmail.com>
2025-10-20 13:13:49 +0330

    setting up

--- Client/src/App.tsx
+++ Client/src/App.tsx
@@ -1,5 +1,14 @@
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import WelcomePage from "./pages/authentication/welcomePage";
+
 function App() {
-  return <></>;
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<WelcomePage />} />
+      </Routes>
+    </Router>
+  );
 }
 
 export default App;
