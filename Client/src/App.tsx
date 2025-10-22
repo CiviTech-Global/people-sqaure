bcbf309ba6b5301a3a44e2007923852f6c327a39
Mohammad Khalilzadeh <muhammadkhalilzadeh1998@gmail.com>
2025-10-22 13:11:44 +0330

    adding presentation layer

--- Client/src/App.tsx
+++ Client/src/App.tsx
@@ -1,11 +1,11 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import WelcomePage from "./pages/authentication/welcomePage";
-import Login from "./pages/authentication/login";
-import Register from "./pages/authentication/register";
-import ForgotPassword from "./pages/authentication/forgotPassword";
-import ForgotPasswordVerificationCode from "./pages/authentication/forgotPasswordVerificationCode";
-import SetNewPassword from "./pages/authentication/setNewPasswprd";
-import HomePage from "./pages/home";
+import WelcomePage from "./presentation/pages/authentication/welcomePage";
+import Login from "./presentation/pages/authentication/login";
+import Register from "./presentation/pages/authentication/register";
+import ForgotPassword from "./presentation/pages/authentication/forgotPassword";
+import ForgotPasswordVerificationCode from "./presentation/pages/authentication/forgotPasswordVerificationCode";
+import SetNewPassword from "./presentation/pages/authentication/setNewPasswprd";
+import HomePage from "./presentation/pages/home";
 
 function App() {
   return (
@@ -15,7 +15,10 @@ function App() {
         <Route path="/login" element={<Login />} />
         <Route path="/register" element={<Register />} />
         <Route path="/forgot-password" element={<ForgotPassword />} />
-        <Route path="/forgot-password-verification" element={<ForgotPasswordVerificationCode />} />
+        <Route
+          path="/forgot-password-verification"
+          element={<ForgotPasswordVerificationCode />}
+        />
         <Route path="/set-new-password" element={<SetNewPassword />} />
         <Route path="/home" element={<HomePage />} />
       </Routes>
